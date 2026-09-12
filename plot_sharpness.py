51439c135ed4f5cef09f78c09f028c4d7d76bf62
Adil Arya <SCHOOL_EMAIL>
2026-09-11 20:12:31 -0500

    perturbation plot

--- plot_sharpness.py
+++ plot_sharpness.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import os
 
 def plot_perturbation(sharpness_config, all_stats):
     for key, values in all_stats.items():
@@ -16,3 +17,19 @@ def plot_perturbation(sharpness_config, all_stats):
     plt.legend()
     plt.savefig(f"{sharpness_config['output_dir']}/perturbation_sharpness.png")
     plt.close()
+
+if __name__ == "__main__":
+
+    sharpness_plots_config = {
+        "output_dir": "sharpness"
+    }
+
+    # making the plots directory
+    os.makedirs(sharpness_plots_config["output_dir"], exist_ok=True)
+
+    # loading the json file for sharpness evals
+    import json
+    with open(f"{sharpness_plots_config['output_dir']}/results.json", "r") as f:
+        all_stats = json.load(f)
+
+    plot_perturbation(sharpness_plots_config, all_stats)
