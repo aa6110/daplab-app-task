80e3ee3c865c4a9fca946f46f38c9330f1c80706
Adil Arya <SCHOOL_EMAIL>
2026-09-11 20:05:07 -0500

    fixed perturbation function

--- sharpness.py
+++ sharpness.py
@@ -183,11 +183,11 @@ def plot_perturbation(sharpness_config, all_stats):
         means  = [values[s]["mean"] for s in values]
         stds   = [values[s]["std"]  for s in values]
         plt.errorbar(sigmas, means, yerr=stds, label=key)
-    plt.xlabel("Perturbation Index")
+    plt.xlabel("Relative Perturbation")
     plt.xscale("log")
-    plt.ylabel("Sharpness")
+    plt.ylabel("Loss Increase")
     plt.yscale("log")
-    plt.title("Perturbation Sharpness")
+    plt.title("Relative Perturbation vs Loss Increase")
     plt.legend()
     plt.savefig(f"{sharpness_config['output_dir']}/perturbation_sharpness.png")
     plt.close()
